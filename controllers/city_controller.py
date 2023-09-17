f7a77ecc8f29b652230353b716764d641fceb7fc
DomonkosNemeth <n.domey@gmail.com>
2023-09-17 23:29:42 +0100

    added function to controller file to display data from db

--- controllers/city_controller.py
+++ controllers/city_controller.py
@@ -4,3 +4,9 @@ from models.country import Country
 from app import db
 
 city_blueprint = Blueprint("cities", __name__)
+
+@city_blueprint.route("/cities")
+def get_cities():
+    cities_from_db = City.query.all()
+    countries_from_db = Country.query.all()
+    return render_template("base.jinja", cities=cities_from_db, countries=countries_from_db)
